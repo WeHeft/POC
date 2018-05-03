70921ffc382b97f57a341aa03158a38befe9a2bb
Stefan <theaichholzer@gmail.com>
2018-05-03 15:32:48 +1000

    Meassure bandwith/speed.

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const express = require('express');
 const { createGunzip } = require('zlib');
 const { createWriteStream } = require('fs');
 
+const speed = (time, bytesReceived) => {
+    const units = ['bps', 'kbps', 'mbps', 'tbps'];
+    ({ 0: secs, 1: nanoSecs } = process.hrtime(time));
+    nanoSecs /= 1000000000;
+    let bpsReceived = Math.ceil(bytesReceived / (secs + nanoSecs)) || 0;
+    ({ 0: unit } = units);
+    let i = 0;
+    while (bpsReceived > 1024 && ++i) {
+      unit = units[i];
+      bpsReceived /= 1024;
+    }
+
+    return `${Math.round(bpsReceived)} ${unit}`;
+};
+
 const app = express();
 app.post('/:fileName/:fileSize?', (req, res) => {
+  const time = process.hrtime();
   let bytesReceived = 0;
   const { fileName, fileSize } = req.params;
   req
@@ -12,13 +28,13 @@ app.post('/:fileName/:fileSize?', (req, res) => {
       const percent = Math.ceil(((bytesReceived * 100) / fileSize).toFixed(2));
       process.stdout.clearLine();
       process.stdout.cursorTo(0);
-      process.stdout.write(`Receiving: ${fileName} (${percent}%) `);
+      process.stdout.write(`Receiving: ${fileName} (${percent}% @ ${speed(time, bytesReceived)}) `);
     })
     .on('end', () => {
-      res.send(JSON.stringify({ bytesReceived }));
+      res.send({ bytesReceived });
       console.log('✔ \n');
     })
-    .on('error', (error) => res.send(JSON.stringify({ error: error.message })))
+    .on('error', (error) => res.send({ error: error.message }))
     .pipe(createGunzip())
     .pipe(createWriteStream(`./received_${fileName}`));
 });
